06aa02493d238d02288beac82defbe162154996b
Mac Meldi Roch <mmeldiroch@gmail.com>
2022-05-05 17:19:35 -0300

    Se optimiza codigo mediante la clase Container, el cual contiene los metodos comunes del Cart y del ProductList

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,67 +1,14 @@
 const fs = require('fs');
+const { Container } = require('./Container');
 
-class Cart{
+class Cart extends Container {
     constructor(id, fileName) {
+        super(fileName);
         this.id = id;
-        this.fileName = fileName;
     }
 
     getId(){
         return this.id;
-    }
-
-    async getAll() {
-        try {
-            const texto = await fs.promises.readFile(this.fileName, {encoding: 'utf-8', flag: 'a+'});
-            if (texto === "") {
-                return [];
-            }
-            return JSON.parse(texto); 
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async save(product) {
-        try {
-            const products = await this.getAll();
-            const newId = products.length + 1;
-            product.id = newId;
-            products.push(product);
-            const newTexto = JSON.stringify(products)
-            await fs.promises.writeFile(this.fileName, newTexto);
-            return newId;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async getById(idIn) {
-        const id = parseInt(idIn);
-        try {
-            const products = await this.getAll();
-            const found = products.find(product => product.id === id);
-            return found ? found : null;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async deleteById(idIn) {
-        const id = parseInt(idIn);
-        try {
-            const products = await this.getAll();
-            const foundIndex = products.findIndex(product => product.id === id);
-            if (foundIndex === -1) {
-                return false;
-            }
-            products.splice(foundIndex, 1);
-            const newTexto = JSON.stringify(products)
-            await fs.promises.writeFile(this.fileName, newTexto);
-            return true
-        } catch (error) {
-            console.log(error);
-        }
     }
 
     async reset() {
--- src/components/productsList.js
+++ src/components/productsList.js
@@ -1,36 +1,8 @@
 const fs = require('fs');
+const { Container } = require('./Container');
 
-class ProductList{
-    constructor(fileName) {
-        this.fileName = fileName;
-    }
-
-    async getAll() {
-        try {
-            const texto = await fs.promises.readFile(this.fileName, {encoding: 'utf-8', flag: 'a+'});
-            if (texto === "") {
-                return [];
-            }
-            return JSON.parse(texto); 
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async save(product) {
-        try {
-            const products = await this.getAll();
-            const newId = products.length + 1;
-            product.id = newId;
-            products.push(product);
-            const newTexto = JSON.stringify(products)
-            await fs.promises.writeFile(this.fileName, newTexto);
-            return newId;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
+class ProductList extends Container {
+    
     async update(idIn, price, stock){
         const id = parseInt(idIn);
         try {
@@ -54,35 +26,6 @@ class ProductList{
             console.log(error);
         }
     }
-
-    async getById(idIn) {
-        const id = parseInt(idIn);
-        try {
-            const products = await this.getAll();
-            const found = products.find(product => product.id === id);
-            return found ? found : null;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async deleteById(idIn) {
-        const id = parseInt(idIn);
-        try {
-            const products = await this.getAll();
-            const foundIndex = products.findIndex(product => product.id === id);
-            if (foundIndex === -1) {
-                return false;
-            }
-            products.splice(foundIndex, 1);
-            const newTexto = JSON.stringify(products)
-            await fs.promises.writeFile(this.fileName, newTexto);
-            return true
-        } catch (error) {
-            console.log(error);
-        }
-    }
 }
-
 
 module.exports = {ProductList};
