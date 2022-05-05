ceaeeb857d80d8ccbc59190394f8d357a2e6ac80
Mac Meldi Roch <mmeldiroch@gmail.com>
2022-05-05 07:57:11 -0300

    Se modifica la clase Cart para que su contenido persista en archivo local

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,45 +1,75 @@
+const fs = require('fs');
+
 class Cart{
-    constructor(id) {
+    constructor(id, fileName) {
         this.id = id;
-        this.products = [];
+        this.fileName = fileName;
     }
 
     getId(){
         return this.id;
     }
 
-    getAll() {
-        return this.products; 
-    }
-
-    save(product) {
-        let newId = 1;
-        if (this.products.length > 0 ){
-            newId = this.products[this.products.length - 1].id + 1;
+    async getAll() {
+        try {
+            const texto = await fs.promises.readFile(this.fileName, {encoding: 'utf-8', flag: 'a+'});
+            if (texto === "") {
+                return [];
+            }
+            return JSON.parse(texto); 
+        } catch (error) {
+            console.log(error);
         }
-        product.id = newId;
-        this.products.push(product);
-        return newId;
     }
 
-    getById(id) {
-        const idNumber = parseInt(id);
-        const found = this.products.find(product => product.id === idNumber);
-        return found ? found : null;
-    }
-
-    deleteById(id) {
-        const idNumber = parseInt(id);
-        const foundIndex = this.products.findIndex(product => product.id === idNumber);
-        if (foundIndex === -1) {
-            return false;
+    async save(product) {
+        try {
+            const products = await this.getAll();
+            const newId = products.length + 1;
+            product.id = newId;
+            products.push(product);
+            const newTexto = JSON.stringify(products)
+            await fs.promises.writeFile(this.fileName, newTexto);
+            return newId;
+        } catch (error) {
+            console.log(error);
         }
-        this.products.splice(foundIndex, 1);
-        return true
     }
 
-    reset() {
-        this.products = [];
+    async getById(idIn) {
+        const id = parseInt(idIn);
+        try {
+            const products = await this.getAll();
+            const found = products.find(product => product.id === id);
+            return found ? found : null;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async deleteById(idIn) {
+        const id = parseInt(idIn);
+        try {
+            const products = await this.getAll();
+            const foundIndex = products.findIndex(product => product.id === id);
+            if (foundIndex === -1) {
+                return false;
+            }
+            products.splice(foundIndex, 1);
+            const newTexto = JSON.stringify(products)
+            await fs.promises.writeFile(this.fileName, newTexto);
+            return true
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async reset() {
+        try {
+            await fs.promises.writeFile(this.fileName, "");
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
