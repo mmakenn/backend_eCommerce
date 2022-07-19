b7458145337e2a96c5ef0a7869f3bd9a46d47028
Macarena Meldi <macarena.meldi@eynes.com.ar>
2022-07-19 17:23:13 -0300

    Se define el schema de usuario con los datos especificados

--- src/containers/usersContainer.js
+++ src/containers/usersContainer.js
@@ -5,7 +5,11 @@ const usersSchema = new mongoose.Schema(
     {
         username: { type: String, required: true }, 
         password: { type: String, required: true },
-        email: { type: String, required: true }
+        name: { type: String, required: true },
+        adress: { type: String, required: true },
+        age: { type: Number, required: true },
+        phone: { type: String, required: true },
+        avatar: { type: String}
     }
 );
 
